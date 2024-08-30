11d1fc4df66f363a27d70a9dea6789ce762af8f8
SachinBishnoi-503 <bishnoisachin626@gmail.com>
2024-08-30 12:46:54 +0530

    update SwioerSlider

--- src/components/view/Home.jsx
+++ src/components/view/Home.jsx
@@ -3,13 +3,15 @@ import Hero from "../home/Hero";
 import Form from "../home/Form";
 import Footer from "../../common/Footer";
 import Navbar from "../../common/Navbar";
+import SwiperSlider from "../home/SwiperSlider";
 const Home = () => {
     return (
         <>
             <Navbar />
             <Header className="bg-lineargradiant" />
             <Hero />
-            <Form />
+            <SwiperSlider />
+            {/* <Form /> */}
             <Footer />
         </>
     )
